4ddc0b6584b66acf54b766f3b59a3947860c61f9
Nitya-N-Hegde20 <108052615+Nitya-N-Hegde20@users.noreply.github.com>
2023-09-13 20:41:57 +0530

    Create Dqe.java

---
+++ Dqe.java
@@ -0,0 +1,13 @@
+package wel;
+import java.util.*;
+public class Dqe {
+
+	public static void main(String[] args) {
+		Deque <Integer>dq= new ArrayDeque<>();
+		dq.addFirst(21);
+		dq.addLast(22);
+		System.out.println(dq);
+
+	}
+
+}
